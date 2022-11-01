edb0bfbcbc58bd3e8562523626b4b467fdb0f33a
CodingTil <36734749+CodingTil@users.noreply.github.com>
2022-11-01 16:48:50 +0100

    wip

--- exercise_1/q5_knn_python/kde.py
+++ exercise_1/q5_knn_python/kde.py
@@ -11,4 +11,20 @@ def kde(samples, h):
 
     #####Insert your code here for subtask 5a#####
     # Compute the number of samples created
+    pos = np.arange(-5, 5, 0.1)
+    estDensity = np.zeros((len(pos), 2))
+    for i in range(len(pos)):
+        estDensity[i, 0] = pos[i]
+
+    for i in range(len(pos)):
+        x = pos[i]
+        est = 0
+        for x_n in samples:
+            est += k(np.abs(x - x_n), h)
+        estDensity[i, 1] = est / len(samples)
+
     return estDensity
+
+
+def k(u, h):
+    return 1 / np.sqrt(2 * np.pi * (h**2)) * np.exp(-(u**2 / (2 * (h**2))))
--- exercise_1/q5_knn_python/knn.py
+++ exercise_1/q5_knn_python/knn.py
@@ -11,4 +11,18 @@ def knn(samples, k):
 
     #####Insert your code here for subtask 5b#####
     # Compute the number of the samples created
+    pos = np.arange(-5, 5, 0.1)
+    estDensity = np.zeros((len(pos), 2))
+    for i in range(len(pos)):
+        estDensity[i, 0] = pos[i]
+
+    for i in range(len(pos)):
+        x = pos[i]
+        estDensity[i, 1] = k / (len(samples) * V(x, samples, k))
+
     return estDensity
+
+
+def V(x, samples, k):
+    sorted_samples = sorted(samples, key=lambda x_n: np.abs(x - x_n))
+    return np.abs(x - sorted_samples[k - 1]) * 2
