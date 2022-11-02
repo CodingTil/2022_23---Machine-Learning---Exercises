66561b7922959970edf5da1bbd29cdeb9749cf35
CodingTil <36734749+CodingTil@users.noreply.github.com>
2022-11-02 16:08:52 +0100

    Q6

--- exercise_1/q6_expectation_maximization_python/parameters.py
+++ exercise_1/q6_expectation_maximization_python/parameters.py
@@ -1,6 +1,6 @@
 def parameters():
     epsilon = 0.0001 # regularization
-    K = 3  # number of desired clusters
+    K = 2  # number of desired clusters
     n_iter = 5  # number of iterations
     skin_n_iter = 5
     skin_epsilon = 0.0001
