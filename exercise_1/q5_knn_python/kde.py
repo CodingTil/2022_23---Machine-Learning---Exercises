4e63f20c8c5f5879ad4be7a6ebabc0c72963708a
simon.michau <simon.michau@live.de>
2022-11-05 13:51:25 +0100

    Added solution for Question 5

--- exercise_1/q5_knn_python/kde.py
+++ exercise_1/q5_knn_python/kde.py
@@ -11,4 +11,24 @@ def kde(samples, h):
 
     #####Insert your code here for subtask 5a#####
     # Compute the number of samples created
+    D = 1
+    N = samples.size
+    pos = np.arange(-5, 5.0, 0.1)
+
+    # Init empty return array
+    estDensity = np.zeros((len(pos), 2))
+    # Init first column
+    estDensity[:, 0] = pos
+
+    # Iterate over first column
+    for id, x in enumerate(estDensity):
+        # calc probability density estimate sum for x
+        sum = 0
+        for i in samples:
+            sum += 1 / (np.power(2 * np.pi, D/2) * h) * np.exp(-np.power(np.linalg.norm(x[0] - i), 2) / (2 * h ** 2))
+        # probability density estimate = 1/N * sum
+        pde = 1/N * sum
+
+        estDensity[id, 1] = pde
+
     return estDensity
--- exercise_1/q5_knn_python/knn.py
+++ exercise_1/q5_knn_python/knn.py
@@ -11,4 +11,28 @@ def knn(samples, k):
 
     #####Insert your code here for subtask 5b#####
     # Compute the number of the samples created
+    D = 1
+    N = samples.size
+    pos = np.arange(-5, 5.0, 0.1)
+
+    # Init empty return array
+    estDensity = np.zeros((len(pos), 2))
+    # Init first column
+    estDensity[:, 0] = pos
+
+    # Find k-nearest-neighbours and store volume V* of the hyperball containing all k points
+    for id, x in enumerate(estDensity):
+        # Find distance k-nearest data point
+        #   Create list of distances between x[0] and {samples}
+        distance_list = []
+        for i in samples:
+            distance_list.append(abs(x[0]-i))
+        #   Sort this list
+        distance_list.sort()
+        print(distance_list[k-1])
+        #   Save V* = 2*"3rd smallest distance"
+        V_star = 2*distance_list[k-1]
+
+        estDensity[id, 1] = k / (N*V_star)
+
     return estDensity
