87dac7e5ec0cd823aec99ccca766227a407fb818
simon.michau <simon.michau@live.de>
2022-11-06 13:23:31 +0100

    Added solution for Question 6a

--- exercise_1/q6_expectation_maximization_python/getLogLikelihood.py
+++ exercise_1/q6_expectation_maximization_python/getLogLikelihood.py
@@ -15,5 +15,25 @@ def getLogLikelihood(means, weights, covariances, X):
     # logLikelihood  : log-likelihood
 
     #####Insert your code here for subtask 6a#####
+    # D=2, N=275, K=3
+    D = X.shape[1]
+    N = X.shape[0]
+    K = len(weights)
+
+    # sum over N data points (rows of X)
+    logLikelihood = 0
+    for n, x in enumerate(X):
+        # sum over K Gaussians (entries in weights)
+        res = 0
+        for k, w in enumerate(weights):
+            # compute gaussian
+            gaussian = 1 / np.sqrt((2 * np.pi) ** D * np.linalg.det(covariances[:, :, k])) \
+                       * np.exp(-0.5 * np.transpose(np.subtract(x, means[k]))
+                                .dot(np.linalg.inv(covariances[:, :, k]))
+                                .dot(np.subtract(x, means[k]))
+                                )
+
+            res += w * gaussian
+        logLikelihood += np.log(res)
     return logLikelihood
 
